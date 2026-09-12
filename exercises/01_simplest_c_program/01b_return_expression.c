8d55ceaa9b17b6ed44ed01aecfd16420f53d8ffc
Noir-0814 <390137880@qq.com>
2026-09-12 16:29:15 +0800

    完成招新考核

--- exercises/01_simplest_c_program/01b_return_expression.c
+++ exercises/01_simplest_c_program/01b_return_expression.c
@@ -15,8 +15,10 @@
  * 验证: clings 检查程序退出码为 3
  */
 
-#error TODO: Fix this exercise. Run "clings hint" for help.
-
+//#error TODO: Fix this exercise. Run "clings hint" for help.
+int global = 2;
 int main(void) {
-#error TODO: Fix this exercise. Run "clings hint" for help.
+    int local = 1;
+//#error TODO: Fix this exercise. Run "clings hint" for help.
+    return local + global;
 }
